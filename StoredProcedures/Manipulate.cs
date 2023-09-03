6095ba65004597828b99f89ecbecbe79dfb1ca99
hyangforest <hyangforest@gmail.com>
2023-09-03 22:24:48 +0900

    SP 데이터 조작용 추가

---
+++ StoredProcedures/Manipulate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TistoryCategoryManager;
+
+public class Manipulate
+{
+    public static void InsertWithParametersAndUpdateSortOrder(AppDbContext context, string spName, HabitCategory parameters)
+    {
+
+        //context?.sp_HabitCategories?.FromSql($"EXEC {spName}").ToList();
+
+    }
+}
